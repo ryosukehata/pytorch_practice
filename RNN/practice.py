f67cf8879ec2c9964b9f3996bf728497ec73eaa9
ryosukehata <hattan0523@gmail.com>
2018-12-24 11:02:51 +0900

    test

---
+++ RNN/practice.py
@@ -0,0 +1,118 @@
+# From https://www.kaggle.com/hengzheng/pytorch-starter
+
+import re
+import time
+import gc
+import random
+import os
+
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from skelern.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.metrics import f1_score, roc_auc_score
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+import torch
+import torch.nn as nn
+import torch.utils.data
+
+
+def seed_torch(seed=1029):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+embed_size = 300 # how big is each word vecror
+max_features = 95000 # how many unique words to use (i.e. num rows is embedding vector)
+maxlen = 72
+
+batch_size = 512
+train_epoch = 6
+
+SEED = 1029
+
+
+puncts = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•',  '~', '@', '£',
+ '·', '_', '{', '}', '©', '^', '®', '`',  '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…',
+ '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─',
+ '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞',
+ '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√', ]
+
+ def clean_text(x):
+     x = re.sub('[0-9]{5,}', '#####', x)
+     x = re.sub('[0-9]{4}', '####', x)
+     x = re.sub('[0-9]{3}', '###', x)
+     x = re.sub('[0-9]{2}', '##', x)
+     return x
+
+mispell_dict = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",  "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",  "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have", 'colour': 'color', 'centre': 'center', 'favourite': 'favorite', 'travelling': 'traveling', 'counselling': 'counseling', 'theatre': 'theater', 'cancelled': 'canceled', 'labour': 'labor', 'organisation': 'organization', 'wwii': 'world war 2', 'citicise': 'criticize', 'youtu ': 'youtube ', 'Qoura': 'Quora', 'sallary': 'salary', 'Whta': 'What', 'narcisist': 'narcissist', 'howdo': 'how do', 'whatare': 'what are', 'howcan': 'how can', 'howmuch': 'how much', 'howmany': 'how many', 'whydo': 'why do', 'doI': 'do I', 'theBest': 'the best', 'howdoes': 'how does', 'mastrubation': 'masturbation', 'mastrubate': 'masturbate', "mastrubating": 'masturbating', 'pennis': 'penis', 'Etherium': 'Ethereum', 'narcissit': 'narcissist', 'bigdata': 'big data', '2k17': '2017', '2k18': '2018', 'qouta': 'quota', 'exboyfriend': 'ex boyfriend', 'airhostess': 'air hostess', "whst": 'what', 'watsapp': 'whatsapp', 'demonitisation': 'demonetization', 'demonitization': 'demonetization', 'demonetisation': 'demonetization'}
+
+def _get_mispell(mispell_dict):
+    mispell_re = re.complie('(%s)' % '|'.join(mispell_dict).keys())
+    return mispell_dict, mispell_re
+
+mispellings, mispellings_re = _get_mispell(mispell_dict)
+
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
+    return mispellings_re.sub(replace, text)
+
+def load_and_prec():
+    train_df = pd.read_csv("../input/train.csv")
+    test_df = pd.read_csv("..input/test.csv")
+    print("Train shape : ", train_df.shape)
+    print("Test shape : ", test_df.shape)
+
+    # lower
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: x.lower())
+    test_df["question_text"] = test_df["question_text"].progress_apply(lambda x: x.lower())
+
+    # Clean the text
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: clean_text(x))
+    test_df["question_text"] = test_df["question_text"].progress_apply(lambda x: clean_text(x))
+
+    # Clean numbers
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: clean_numbers(x))
+    test_df["question_text"] = test_df["question_text"].progress_apply(lambda x: clean_numbers(x))
+
+    # Clean speellings
+    train_df["question_text"] = train_df["question_text"].progress_apply(lambda x: replace_typical_misspell(x))
+    test_df["question_text"] = test_df["question_text"].progress_apply(lambda x: replace_typical_misspell(x))
+
+    # fill up the missing values
+    train_X = train_df["question_text"].fillna("_##_").values
+    test_X = test_df["question_text"].fillna("_##_").values
+
+    # Tokenize the setences
+    tokenizer = Tokenizer(num_words=max_features)
+    tokenizer.fit_on_text(list(train_X))
+    train_X = tokenizer.text_to_sequences(train_X)
+    test_X = tokenizer.text_to_sequences(test_X)
+
+    # Pad the sentences
+    train_X = pad_sequences(train_X, maxlen=maxlen)
+    test_X = pad_sequences(test_X, maxlen=maxlen)
+
+    # Get the target values
+    train_y = train_df["target"].values
+
+    # suffling the data
+    np.random.seed(SEED)
+    trn_idx = np.random.permutation(len(train_X))
+
+    train_X = train_X[trn_idx]
+    train_y = train_y[trn_idx]
+
+    return train_X, test_X, train_y, tokenizer.word_index
+
+  
